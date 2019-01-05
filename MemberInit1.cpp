f75a726df9bdf6719896bda5a994c6d4533ed416
HSUABI <seongwooksong2107@gmail.com>
2019-01-05 19:28:37 +0900

    108p

---
+++ MemberInit1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+class user
+{
+public:
+	user()
+	{
+		int age=10;
+		char name[32]="song";
+	}
+	int age=10;
+	char name[32]="song";
+
+	void print(void)
+	{
+		printf("%d , %s\n",age,name );
+	}
+};
+int main()
+{
+	user song;
+	song.print();
+
+	return 0;
+}
